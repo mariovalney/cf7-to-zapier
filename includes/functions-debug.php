03ed1f443e5db9712a186f629fb079d8e900ec32
Mário Valney <mariovalney@gmail.com>
2023-09-30 20:10:33 -0300

    [Feat] Removing debug functions in production

--- includes/functions-debug.php
+++ includes/functions-debug.php
@@ -9,12 +9,16 @@
 
 defined( 'ABSPATH' ) || die( 'No script kiddies please!' );
 
+function cftz_activated_debug_functions() {
+    return ( defined( 'WP_DEBUG' ) && WP_DEBUG ) && ! ( defined( 'CFTZ_REMOVE_DEBUG_FUNCTIONS' ) && CFTZ_REMOVE_DEBUG_FUNCTIONS );
+}
+
 /**
  * Emulate dd function from laravel
  *
  * @SuppressWarnings(PHPMD)
  */
-if ( ! function_exists( 'dd' ) ) {
+if ( ! function_exists( 'dd' ) && cftz_activated_debug_functions() ) {
     function dd( $param, $include_pre = true ) {
         echo $include_pre ? '<pre>' : '';
         print_r( $param );
@@ -27,7 +31,7 @@ if ( ! function_exists( 'dd' ) ) {
  * Emulate dump function from
  * laravel but write to logs
  */
-if ( ! function_exists( 'dump' ) ) {
+if ( ! function_exists( 'dump' ) && cftz_activated_debug_functions() ) {
     function dump( $param ) {
         error_log( $param );
     }
